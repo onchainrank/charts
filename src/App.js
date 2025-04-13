b3a4de993c8dd2434927edf97b68039b4939432a
jac stn <jacek.stanusz@gmailwq.com>
2025-04-13 11:30:05 +0200

    remove main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./HomePage";
 import SingleChartPage from "./SingleChartPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
       <Route path="/single/:id" element={<SingleChartPage />} />
     </Routes>
   );
