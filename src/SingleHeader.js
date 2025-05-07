0ae7846fe593c1a57cdbe11fbd95b34a39ecd303
jac stn <jacek.stanusz@gmailwq.com>
2025-05-07 22:43:08 +0200

    unique socials icon

--- src/SingleHeader.js
+++ src/SingleHeader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ValidLaunchIcon from "./components/ValidLaunchIcon";
 import ValidSocialsIcon from "./components/ValidSocialsIcon";
+import UniqueSocialsIcon from "./components/UniqueSocialsIcon";
 const SingleHeader = ({
   recentCSolVal,
   max_cactor_rank,
@@ -58,29 +59,12 @@ const SingleHeader = ({
       )}
 
       {/* valid_launch logic */}
-      {valid_launch === false}
-      <ValidLaunchIcon />
+      {valid_launch === false && <ValidLaunchIcon />}
+
       {valid_launch === true && (
         <>
           {!valid_socials && <ValidSocialsIcon />}
-          {!unique_socials && (
-            <span
-              className="ms-2"
-              title="Social account already used: duplicate pages detected."
-              style={{ marginRight: 8, cursor: "help" }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="red"
-                className="bi bi-twitter blinking-icon"
-                viewBox="0 0 16 16"
-              >
-                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15" />
-              </svg>
-            </span>
-          )}
+          {!unique_socials && <UniqueSocialsIcon />}
         </>
       )}
       {(bundle_ratio) =>
